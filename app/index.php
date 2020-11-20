b458d92eb91ca22158fb5461bdc6f2e4c1e283eb
david-salac <info@davidsalac.eu>
2020-11-20 21:04:26 +0000

    STYLES added

--- app/index.php
+++ app/index.php
@@ -5,13 +5,40 @@ header('Content-type: application/json; charset=utf-8', false);
 $LINK_TO_JSON = "questions.json";
 $SUCCESS_MESSAGE = "Email sent!";
 
+/* ====================== CONTENT DEFINITION =============================== */
+define("ADDRESSING_PREFIX", "Dear ");
+define("ADDRESSING_SUFFIX", ",<p>Here is the certificate from our quiz.</p>");
+define("CONTENT_SUFFIX", "<p>End of the content.</p>");
+define("QUESTION_SCORE_PREFIX", "Score for question: ");
+define("QUESTION_SCORE_SUFFIX", ".");
+define("TOTAL_SCORE_PREFIX", "Total score: ");
+define("TOTAL_SCORE_SUFFIX", " percent.");
+/* ========================================================================= */
+
+
+/* ========================= CSS STYLES FOR ELEMENTS ======================= */
+define("QUESTION_STYLE", "margin: 0;");
+define("OPTION_CORRECT_STYLE", "background: #00C521; padding: 15px; box-sizing: border-box;");
+define("OPTION_INCORRECT_STYLE", "background: #FEA4A4; padding: 15px; box-sizing: border-box;");
+define("OPTION_CORRECT_UNSELECTED_STYLE", "background: #B4F0BE; padding: 15px; box-sizing: border-box;");
+define("OPTION_OTHER_STYLE", "background: #dedede; padding: 15px; box-sizing: border-box;");
+define("QUESTION_WRAPPER_STYLE", "background: #F5F5F5; padding: 15px 20px 15px 20px; box-sizing: border-box; margin-bottom: 20px;");
+define("QUESTION_SCORE_STYLE", "margin-bottom: 0; padding-bottom: 0;");
+define("ALL_QUESTIONS_WRAPPER_STYLE", "padding: 15px; box-sizing: border-box; border: 1px solid #EAEAEA; border-radius: 5px; margin-top: 15px");
+define("TOTAL_SCORE_STYLE", "margin: 0;");
+define("CONTENT_WRAPPER_STYLE", "background: #ffffff; padding: 20px; box-sizing: border-box;");
+define("LAYOUT_STYLE", "margin: auto; width: 90%; max-width: 900px; font-family: sans-serif;");
+/* ========================================================================= */
+
 /*
 TODO:
-1. Styles
+0. All to config
+1. Styles (common + specific)
 1.a. Passing name/email as questions wrapper arg.
 2. Storing to file
 3. Body wrapper
 4. Sending to email.
+5. Generate description (legend)
 */
 
 // =================== RENDER FRACTION OF CODE FOR RESPONSE ===================
@@ -21,7 +48,7 @@ function render_html_question($question_text, $order) {
     @param order: Relative position (indexed from 1).
     @return: HTML string defining the question.
     */
-    return "<h2>".$order.") ".$question_text."</h2>";
+    return '<h2 style="'.QUESTION_STYLE.'">'.$order.") ".$question_text."</h2>";
 }
 function render_html_option_correct($option_text, $order) {
     /**Correct answer selected by user
@@ -29,7 +56,7 @@ function render_html_option_correct($option_text, $order) {
     @param order: Relative position (indexed from 1).
     @return: HTML string defining the option.
     */
-    return "<strong style='background: #31AD00;'>".$option_text."</strong><br>";
+    return '<p style="'.OPTION_CORRECT_STYLE.'">'.$option_text."</p>";
 }
 function render_html_option_incorrect($option_text, $order) {
     /**Incorrect answer selected by user
@@ -37,7 +64,7 @@ function render_html_option_incorrect($option_text, $order) {
     @param order: Relative position (indexed from 1).
     @return: HTML string defining the option.
     */
-    return "<strong style='background: red;'>".$option_text."</strong><br>";
+    return '<p style="'.OPTION_INCORRECT_STYLE.'">'.$option_text."</p>";
 }
 function render_html_option_unselected_correct($option_text, $order) {
     /**Correct answer NOT selected by user
@@ -45,7 +72,7 @@ function render_html_option_unselected_correct($option_text, $order) {
     @param order: Relative position (indexed from 1).
     @return: HTML string defining the option.
     */
-    return "<strong style='background: #B7EBA2;'>".$option_text."</strong><br>";
+    return '<p style="'.OPTION_CORRECT_UNSELECTED_STYLE.'">'.$option_text."</p>";
 }
 function render_html_option_others($option_text, $order) {
     /**Incorrect unselected answer
@@ -53,15 +80,38 @@ function render_html_option_others($option_text, $order) {
     @param order: Relative position (indexed from 1).
     @return: HTML string defining the option.
     */
-    return "<strong>".$option_text."</strong><br>";
+    return '<p style="'.OPTION_OTHER_STYLE.'">'.$option_text."</p>";
 }
 function render_question_options_wrapper($question_plus_options, $score) {
-    /**Wrapper for question plus option*/
-    return '<div style="background: #ededed; margin-bottom: 20px">'.$question_plus_options."<h3>Score: ".number_format($score, 2)."</h3></div>";
+    /**Wrapper for question plus option. Mainly for adding score to one
+        question and visually separate questions.
+    @param question_plus_options: single HTML question code.
+    @param score: score received for answering this particular question.
+    */
+    return '<div style="'.QUESTION_WRAPPER_STYLE.'">'.$question_plus_options.'<h3 style="'.QUESTION_SCORE_STYLE.'">'.QUESTION_SCORE_PREFIX.number_format($score, 2).QUESTION_SCORE_SUFFIX."</h3></div>";
 }
 function render_all_questions_wrapper($all_questions, $total_score, $total_question_number) {
-    /**Wrapper for all question plus options*/
-    return '<div style="background: #efefef; padding: 20px">'.$all_questions."<h3>Total score: ".number_format(100 * $total_score/$total_question_number, 2)." percent</h3></div>";
+    /**Wrapper for all question plus options. Mainly for adding total score.
+    @param all_questions: HTML code of questions.
+    @param total_score: Total score of the user.
+    @param total_question_number: Total number (count) of questions.
+    */
+    return '<div style="'.ALL_QUESTIONS_WRAPPER_STYLE.'">'.$all_questions.'<h2 style="'.TOTAL_SCORE_STYLE.'">'.TOTAL_SCORE_PREFIX.number_format(100 * $total_score/$total_question_number, 2).TOTAL_SCORE_SUFFIX."</h2></div>";
+}
+function render_content_wrapper($content, $name_of_user, $address_of_user) {
+    /**Wrap the content of pages (technically all questions). Mainly for adding
+        letter to user.
+    @param content: HTML content of pages.
+    @param name_of_user: Name of the user (like John).
+    @param address_of_user: e-mail address of user.
+    */
+    return '<div style="'.CONTENT_WRAPPER_STYLE.'">'.ADDRESSING_PREFIX.$name_of_user.ADDRESSING_SUFFIX.$content.CONTENT_SUFFIX."</div>";
+}
+function render_layout_wrapper($page_content) {
+    /**Wrap the page content for layout. Mainly for layout
+    @param page_content: HTML content of pages.
+    */
+    return '<html><body><div style="'.LAYOUT_STYLE.'">'.$page_content."</div></body></html>";
 }
 // ============================================================================
 
@@ -84,7 +134,7 @@ function get_list_of_post_keys($decoded_json) {
     return $list_of_options;
 }
 
-function prepare_result_message($decoded_json, $selected_values) {
+function prepare_result_message($decoded_json, $selected_values, $name_of_user, $address_of_user) {
     /**Prepare the HTML email response.
 
     @param decoded_json: JSON dataclass with all questions and answers.
@@ -140,9 +190,12 @@ function prepare_result_message($decoded_json, $selected_values) {
         $questions_code .= render_question_options_wrapper($html_question, $score);
         $total_score += $score;
     }
-    $html_code = render_all_questions_wrapper($questions_code, $total_score, $question_order);
+    $content = render_all_questions_wrapper($questions_code, $total_score, $question_order);
+    $page_body = render_content_wrapper($content, $name_of_user, $address_of_user);
+    $whole_layout = render_layout_wrapper($page_body);
 
-    return $html_code;
+    // Return whole HTML code
+    return $whole_layout;
 }
 
 
@@ -202,5 +255,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     header('HTTP/1.1 200 OK', false, 200);
     //exit(json_encode(array("message" => $SUCCESS_MESSAGE)));
 
-    exit(json_encode(array("message" => prepare_result_message($json_decoded, $selected_values))));
+    exit(json_encode(array("message" => prepare_result_message($json_decoded, $selected_values, $name_of_user, $address))));
 }
